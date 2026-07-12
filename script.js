7a371699e6bb082e9bc019b88e0dca4602388bc0
Novawallpaper <shopifly09@gmail.com>
2026-07-12 18:18:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ function ensureAdCounterFresh() {
 const COLLECTIONS = [
   { key: "Space", title: "Aero Space", img: "https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?w=900" },
   { key: "Cyber", title: "Cyber", img: "https://images.unsplash.com/photo-1614730321146-b6fa6a46bcb4?w=900" },
-  { key: "Nature", title: "Nature", img: "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?w=900" },
+  { key: "Nature", title: "Nature", img: "https://novawallpaper.github.io/depthx-app/nature-001.jpg" },
   { key: "Gaming", title: "Gaming", img: "https://images.unsplash.com/photo-1511512578047-dfb367046420?w=900" },
   { key: "Anime", title: "Anime Vibes", img: "https://images.unsplash.com/photo-1578632767115-351597cf2477?w=900" },
 ];
